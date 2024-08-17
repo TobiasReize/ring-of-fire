3012701233db9fd709fa7693b3440d8a908e2382
Tobias Reize <reizeto@web.de>
2024-08-17 12:41:07 +0200

    added game.ts and newGame- and takeCard-function

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,18 +1,46 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrl: './game.component.scss',
 })
-export class GameComponent {
-
+export class GameComponent implements OnInit {
   pickCardAnimation = false;
+  game!: Game;
+  currentCard: string = '';
+
+
+  constructor() {}
+
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
 
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      this.pickCardAnimation = true;
+      this.currentCard = this.game.stack.pop()!;  // "!" damit der Typ "undefined" entfernt wird. Ist das good oder bad practice?
+        console.log(this.currentCard);
+      this.game.playedCards.push(this.currentCard);
+
+      setTimeout(() => {
+        this.pickCardAnimation = false;
+      }, 1500);
+    }
   }
+
+
 }
